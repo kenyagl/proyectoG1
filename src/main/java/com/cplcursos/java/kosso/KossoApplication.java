872ff4d03a9c365a7a2e5f5371989695a93fbc0c
CPL <carlos@centropyme.com>
2023-03-14 18:07:03 +0100

    Commit inicial según el archivo generado desde spring.io/initializr

---
+++ src/main/java/com/cplcursos/java/kosso/KossoApplication.java
@@ -0,0 +1,13 @@
+package com.cplcursos.java.kosso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KossoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KossoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cplcursos/java/kosso/KossoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cplcursos.java.kosso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KossoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

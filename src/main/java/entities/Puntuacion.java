161728c9c05056cf901abab0729e35d62a27ef39
prbm13 <123487449+prbm13@users.noreply.github.com>
2023-03-22 18:16:36 +0100

    KEG1-22 Se crea la entidad Puntuacion con su controlador y su repositorio

---
+++ src/main/java/entities/Puntuacion.java
@@ -0,0 +1,30 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "puntuacion")
+public class Puntuacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Puntuacion", nullable = false)
+    private Long id;
+
+    private Long puntosTotales;
+    private Long puntosEjercicios;
+    private Long puntosRespuestas;
+
+    public Puntuacion(Long puntosTotales, Long puntosEjercicios, Long puntosRespuestas) {
+        this.puntosTotales = puntosTotales;
+        this.puntosEjercicios = puntosEjercicios;
+        this.puntosRespuestas = puntosRespuestas;
+    }
+}

3ed2467573ca0f84e4debb4205c5795310ab6878
Felipe Parra <43170148+FeliParra@users.noreply.github.com>
2023-04-14 16:51:50 +0200

    Controllers mapping

--- src/main/java/controllers/ControladorEtiquetas.java
+++ src/main/java/controllers/ControladorEtiquetas.java
@@ -1,11 +1,26 @@
 package controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/etiquetas")
 public class ControladorEtiquetas {
+
+    @GetMapping("")
+    public String getEtiqueta (Model model){
+        return "etiqueta";
+    }
+
+    @PostMapping("")
+    public String selectEtiqueta (Model model){
+        return "etiqueta";
+    }
 }
--- src/main/java/controllers/ControladorTiposPerfil.java
+++ src/main/java/controllers/ControladorTiposPerfil.java
@@ -1,12 +1,22 @@
 package controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/tiposPerfil")
 public class ControladorTiposPerfil {
+
+    @PostMapping("")
+    public String selectTipoPerfil (Model model){
+        return "perfil";
+    }
 
 }

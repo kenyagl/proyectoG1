c17484fe87667299f742ca25b687028ca0a8ec0c
Kenya <112879619+kenyagl@users.noreply.github.com>
2023-04-15 07:24:59 +0200

    KEG1-41 - borro lo que hay aquí en controlador ejercicios para que luego no haya conflictos

--- src/main/java/controllers/CategoriaEjerciciosController.java
+++ src/main/java/controllers/CategoriaEjerciciosController.java
@@ -1,26 +1,11 @@
 package controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
-@RequestMapping("/ejercicios")
 public class CategoriaEjerciciosController {
-
-    @GetMapping("")
-    public String selectEjercicio (Model model){
-        return "ejercicio";
-    }
-
-    @PostMapping("")
-    public String createEjercicio (Model model){
-        return "ejercicio";
-    }
 }

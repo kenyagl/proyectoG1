53808dcc68f180453c941bc6c5feeadfdd0b6d9c
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-04-24 01:38:40 +0200

    Create: -@Embeddable class RespuestaEjId that refers "usuario_id" and "fecha_id" as multiple PK
    -Entity and @Table "RespuestaEjerciciosOpMul"
    
    -@EmbeddedId RespuestaEjId

--- src/main/java/com/cplcursos/java/kosso/entities/RespuestaEjOpMul.java
+++ src/main/java/com/cplcursos/java/kosso/entities/RespuestaEjOpMul.java
@@ -6,19 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "RespuestaEjerciciosOpMul")
 public class RespuestaEjOpMul {
     @EmbeddedId
     private IdRespuestaEj idRespuestaEj;
     //El id es doble y se encuentra en idRespuestaEj: ahí tienes la fecha metida
+    @EmbeddedId
+    private RespuestaEjId id;
 
     private String respuesta;
+    private Integer respuesta;
 
     private LocalDateTime fechaRespuesta;
+    @ManyToOne
+    @JoinColumn(name = "id_ejercicioOpMul", nullable = false)
+    private EjercicioOpMul ejercicioOpMul;
 }

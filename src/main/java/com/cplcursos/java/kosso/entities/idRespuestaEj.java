6ad0bc2ad86c689dc1a9bd3ea4cc223c25b39aba
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-04-23 23:30:31 +0200

    Correct:
    DateTime -> LocalDate
    
    Import:
    
    import java.oi.Serializable;

--- src/main/java/com/cplcursos/java/kosso/entities/idRespuestaEj.java
+++ src/main/java/com/cplcursos/java/kosso/entities/idRespuestaEj.java
@@ -2,10 +2,13 @@ package com.cplcursos.java.kosso.entities;
 
 import com.fasterxml.jackson.core.SerializableString;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
 public class idRespuestaEj implements Serializable {
 
     //quiero que el idUsuario venga de una relación many to one con la tabla Usuario, pero como es otra clase para
     //el doble id no sé cómo hacerlo
     private Long idUsuario;
-    private DateTime fecha;
+    private LocalDate fecha;
 }

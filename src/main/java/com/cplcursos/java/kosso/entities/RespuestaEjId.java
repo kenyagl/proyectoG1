862aed14aa32a8ca6e45515ee3574e8562232822
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-04-24 01:40:15 +0200

    Create: -@Embeddable class RespuestaEjId that refers "usuario_id" and "fecha_id" as multiple PK
    -Entity and @Table "RespuestaEjerciciosOpMul"
    
    -@EmbeddedId RespuestaEjId

--- src/main/java/com/cplcursos/java/kosso/entities/RespuestaEjId.java
+++ src/main/java/com/cplcursos/java/kosso/entities/RespuestaEjId.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 
 @Embeddable
 public class RespuestaEjId implements Serializable {
-    // TODO create Many-to-one relationship with Usuario table and create an embedabble id
     @Column(name = "usuario_id")
     private Long usuarioId;
     @Column(name = "fecha_id")

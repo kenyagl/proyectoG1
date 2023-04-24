74cad2baf3663dd74b79b4ab54c3010480898cf1
Felipe Parra <43170148+FeliParra@users.noreply.github.com>
2023-04-24 16:14:05 +0200

    Puntuacion ctrl, srvc y repo

--- src/main/java/com/cplcursos/java/kosso/repositories/PuntuacionRepo.java
+++ src/main/java/com/cplcursos/java/kosso/repositories/PuntuacionRepo.java
@@ -2,7 +2,12 @@ package com.cplcursos.java.kosso.repositories;
 
 import com.cplcursos.java.kosso.entities.Puntuacion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface PuntuacionRepo extends JpaRepository<Puntuacion, Long> {
+
+    List<Puntuacion> findByName(@Param("nombre") String nombre);
 }

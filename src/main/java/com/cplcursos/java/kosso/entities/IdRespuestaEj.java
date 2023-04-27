ce0a892a6582b263d95e00f3c616211701a3b86d
Kenya <112879619+kenyagl@users.noreply.github.com>
2023-04-27 19:05:29 +0200

    quito archivos css redundantes y quito fecha de pk de respuesta ejercicio

--- src/main/java/com/cplcursos/java/kosso/entities/IdRespuestaEj.java
+++ src/main/java/com/cplcursos/java/kosso/entities/IdRespuestaEj.java
@@ -15,7 +15,6 @@ public class IdRespuestaEj implements Serializable {
     //quiero que el idUsuario venga de una relación many to one con la tabla Usuario, pero como es otra clase para
     //el doble id no sé cómo hacerlo
     private Long idUsuario;
-    private LocalDate fecha;
     private Long idEjercicioOpMul;
 
 

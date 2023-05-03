bad387d1472b6b14e6bb42433364607aa710903c
Kenya <112879619+kenyagl@users.noreply.github.com>
2023-05-03 18:06:51 +0200

    Arreglo pasar de ej

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,13 +12,13 @@ VALUES ('What is 5 times 6?', '1', '25', '30', '35', '40', '150', '30','Multipli
 INSERT INTO mydb.ejercicio_op_mul (descripcion, dificultad, opcion1, opcion2, opcion3, opcion4, puntos_acceso, respuesta_correcta,titulo)
 VALUES ('What is 8 times 11?', '2', '72', '80', '88', '96', '200', '88','Multiplication Exercise 4');
 INSERT INTO mydb.ejercicio_op_mul (descripcion, dificultad, opcion1, opcion2, opcion3, opcion4, puntos_acceso, respuesta_correcta,titulo)
-VALUES ('What is 12 multiplied by 6?', '2', '62', '68', '72', '76', '250', '3','Multiplication Exercise 5');
+VALUES ('What is 12 multiplied by 6?', '2', '62', '68', '72', '76', '250', '72','Multiplication Exercise 5');
 INSERT INTO mydb.ejercicio_op_mul (descripcion, dificultad, opcion1, opcion2, opcion3, opcion4, puntos_acceso, respuesta_correcta,titulo)
-VALUES ('What is 7 multiplied by 11?', '3', '69', '72', '77', '81', '300', '4','Multiplication Exercise 6');
+VALUES ('What is 7 multiplied by 11?', '3', '69', '72', '77', '81', '300', '77','Multiplication Exercise 6');
 INSERT INTO mydb.ejercicio_op_mul (descripcion, dificultad, opcion1, opcion2, opcion3, opcion4, puntos_acceso, respuesta_correcta,titulo)
-VALUES ('What is 3 multiplied by 13?', '2', '34', '36', '39', '41', '350', '2','Multiplication Exercise 7');
+VALUES ('What is 3 multiplied by 13?', '2', '34', '36', '39', '41', '350', '39','Multiplication Exercise 7');
 INSERT INTO mydb.ejercicio_op_mul (descripcion, dificultad, opcion1, opcion2, opcion3, opcion4, puntos_acceso, respuesta_correcta,titulo)
-VALUES ('What is 18 multiplied by 2?', '1', '20', '30', '36', '42', '400', '3','Multiplication Exercise 8');
+VALUES ('What is 18 multiplied by 2?', '1', '20', '30', '36', '42', '400', '36','Multiplication Exercise 8');
 
 INSERT INTO `mydb`.`categoria_ejercicios` (`nombre`) VALUES ('String');
 INSERT INTO `mydb`.`categoria_ejercicios` (`nombre`) VALUES ('Array');

48d89e3303fdbc0ed33c6afa50061a6ba48285c1
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-05-04 13:11:41 +0200

    Actualiza:
    RespuestaEjOpMulSrvc.java

--- src/main/java/com/cplcursos/java/kosso/services/RespuestaEjOpMulSrvc.java
+++ src/main/java/com/cplcursos/java/kosso/services/RespuestaEjOpMulSrvc.java
@@ -6,6 +6,7 @@ import com.cplcursos.java.kosso.repositories.RespuestaEjOpMulRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +33,8 @@ public class RespuestaEjOpMulSrvc {
 
     public void saveAndFlush(RespuestaEjOpMul respuestaEjOpMul) {
         respuestaEjOpMulRepo.saveAndFlush(respuestaEjOpMul);
+    }
+    public List<RespuestaEjOpMul> getDateRange(LocalDateTime startDate, LocalDateTime endDate) {
+        return respuestaEjOpMulRepo.findByFechaRespuestaBetween(startDate, endDate);
     }
 }

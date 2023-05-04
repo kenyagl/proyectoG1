46865679835cbbd2625c812753466ab0cc9959c5
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-05-04 13:10:05 +0200

    Actualiza:
    ProgresoCtrl.java

--- src/main/java/com/cplcursos/java/kosso/controllers/ProgresoCtrl.java
+++ src/main/java/com/cplcursos/java/kosso/controllers/ProgresoCtrl.java
@@ -1,23 +1,47 @@
 package com.cplcursos.java.kosso.controllers;
 
 
+import com.cplcursos.java.kosso.entities.RespuestaEjOpMul;
+import com.cplcursos.java.kosso.services.RespuestaEjOpMulSrvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Controller
 @RequestMapping("/progreso")
 public class ProgresoCtrl {
-
-    @PostMapping("/progreso")
-    public String inicioTuProgreso (Model model){
+    @Autowired
+    RespuestaEjOpMulSrvc respuestaEjOpMulSrvc;
+    @GetMapping("/progreso-mensual")
+    public String showProgresoMensual(Model model) {
+        LocalDateTime currentDate = LocalDateTime.now();
+        LocalDateTime startDate = currentDate.minusMonths(1);
+        List<RespuestaEjOpMul> agrupaPorMes = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
+        model.addAttribute("agrupaPorMes", agrupaPorMes);
         return "tuProgreso";
     }
 
-    @GetMapping("/progreso")
-    public String consultarProgreso (Model model){
+    @GetMapping("/progreso-semanal")
+    public String showProgresoSemanal(Model model){
+        LocalDateTime currentDate = LocalDateTime.now();
+        LocalDateTime startDate = currentDate.minusWeeks(1);
+        List<RespuestaEjOpMul> agrupaPorSemana = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
+        model.addAttribute("agrupaPorSemana", agrupaPorSemana);
+        return "tuProgreso";
+    }
+
+    @GetMapping("/progreso-diario")
+    public String showProgresoDiario(Model model){
+        LocalDateTime currentDate = LocalDateTime.now();
+        LocalDateTime startDate = currentDate.minusDays(1);
+        List<RespuestaEjOpMul> agrupaPorDia = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
+        model.addAttribute("agrupaPorDia", agrupaPorDia);
         return "tuProgreso";
     }
 }

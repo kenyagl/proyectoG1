67582bc47da6243cb26a51dbdc4c2554dc01625d
maguirre42 <121882599+maguirre42@users.noreply.github.com>
2023-05-05 02:34:13 +0200

    Create:
    -Controllers preliminary version
    -Htmls

--- src/main/java/com/cplcursos/java/kosso/controllers/ProgresoCtrl.java
+++ src/main/java/com/cplcursos/java/kosso/controllers/ProgresoCtrl.java
@@ -1,30 +1,106 @@
 package com.cplcursos.java.kosso.controllers;
 
-
 import com.cplcursos.java.kosso.entities.RespuestaEjOpMul;
 import com.cplcursos.java.kosso.services.RespuestaEjOpMulSrvc;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.time.temporal.IsoFields;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Log4j2
 @Controller
 @RequestMapping("/progreso")
 public class ProgresoCtrl {
     @Autowired
     RespuestaEjOpMulSrvc respuestaEjOpMulSrvc;
+
+    // Returns a list with respuestas log
+    @GetMapping(value = {"/", ""})
+    public String showAllProgreso (Model model){
+        model.addAttribute("respuestas", respuestaEjOpMulSrvc.findAll());
+        return "respuesta";
+    }
+
+    // Returns a view of respuestas grouped by month, day and year with no reference to current day
+    @GetMapping("/progress")
+    public String showProgress(Model model) {
+        // Retrieve the data from the database
+        List<RespuestaEjOpMul> respuestas = respuestaEjOpMulSrvc.findAll();
+        int numeroRespuestas = respuestaEjOpMulSrvc.findAll().size();
+
+        // Calculate the number of respuestas by month, week and day
+        // This shows the progress by month in general with no reference to current date
+        Map<String, Integer> respuestasByMonth = new HashMap<>();
+        Map<String, Integer> respuestasByWeek = new HashMap<>();
+        Map<String, Integer> respuestasByDay = new HashMap<>();
+        for (RespuestaEjOpMul respuesta : respuestas) {
+            String month = respuesta.getFechaRespuesta().getMonth().toString();
+            String week = respuesta.getFechaRespuesta().getYear() + "-Semana: " + respuesta.getFechaRespuesta().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+            String day = respuesta.getFechaRespuesta().toString().substring(0, 10);
+            respuestasByMonth.put(month, respuestasByMonth.getOrDefault(month, 0) + 1);
+            respuestasByWeek.put(week, respuestasByWeek.getOrDefault(week, 0) + 1);
+            respuestasByDay.put(day, respuestasByDay.getOrDefault(day, 0) + 1);
+        }
+
+        // Add the data to the model
+        model.addAttribute("numeroRespuestas", numeroRespuestas);
+        model.addAttribute("respuestasByMonth", respuestasByMonth);
+        model.addAttribute("respuestasByWeek", respuestasByWeek);
+        model.addAttribute("respuestasByDay", respuestasByDay);
+
+        return "progress";
+    }
+    @GetMapping("/usuario-progress")
+    public String showUsuarioProgress(Model model) {
+
+        // Retrieve all RespuestaEjOpMul entities
+        List<RespuestaEjOpMul> respuestas = respuestaEjOpMulSrvc.findAll();
+
+        // Calculate progress for current month, week, and day
+        LocalDateTime now = LocalDateTime.now();
+        int currentMonth = now.getMonthValue();
+        int currentWeek = now.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+        int currentDay = now.getDayOfMonth();
+        int totalAnswersThisMonth = 0;
+        int totalAnswersThisWeek = 0;
+        int totalAnswersToday = 0;
+        for (RespuestaEjOpMul respuesta : respuestas) {
+            LocalDateTime fechaRespuesta = respuesta.getFechaRespuesta();
+            if (fechaRespuesta.getMonthValue() == currentMonth) {
+                totalAnswersThisMonth++;
+            }
+            if (fechaRespuesta.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR) == currentWeek) {
+                totalAnswersThisWeek++;
+            }
+            if (fechaRespuesta.getDayOfMonth() == currentDay) {
+                totalAnswersToday++;
+            }
+        }
+
+        // Add progress data to model
+        model.addAttribute("totalAnswersThisMonth", totalAnswersThisMonth);
+        model.addAttribute("totalAnswersThisWeek", totalAnswersThisWeek);
+        model.addAttribute("totalAnswersToday", totalAnswersToday);
+
+        return "usuario-progress";
+    }
+
     @GetMapping("/progreso-mensual")
     public String showProgresoMensual(Model model) {
+
         LocalDateTime currentDate = LocalDateTime.now();
         LocalDateTime startDate = currentDate.minusMonths(1);
         List<RespuestaEjOpMul> agrupaPorMes = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
         model.addAttribute("agrupaPorMes", agrupaPorMes);
-        return "tuProgreso";
+        return "progresomensual";
     }
 
     @GetMapping("/progreso-semanal")
@@ -33,7 +109,7 @@ public class ProgresoCtrl {
         LocalDateTime startDate = currentDate.minusWeeks(1);
         List<RespuestaEjOpMul> agrupaPorSemana = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
         model.addAttribute("agrupaPorSemana", agrupaPorSemana);
-        return "tuProgreso";
+        return "progresosemanal";
     }
 
     @GetMapping("/progreso-diario")
@@ -42,6 +118,6 @@ public class ProgresoCtrl {
         LocalDateTime startDate = currentDate.minusDays(1);
         List<RespuestaEjOpMul> agrupaPorDia = respuestaEjOpMulSrvc.getDateRange(startDate, currentDate);
         model.addAttribute("agrupaPorDia", agrupaPorDia);
-        return "tuProgreso";
+        return "progresodiario";
     }
 }

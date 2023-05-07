ffdbd6e052ac3887a607ab343b3892045b950e91
Kenya <112879619+kenyagl@users.noreply.github.com>
2023-05-07 11:50:17 +0200

    KEG1-77 - Añado login básico con usuarios hardcoded

--- src/main/java/com/cplcursos/java/kosso/controllers/MainCtrl.java
+++ src/main/java/com/cplcursos/java/kosso/controllers/MainCtrl.java
@@ -3,6 +3,8 @@ package com.cplcursos.java.kosso.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainCtrl {
@@ -15,5 +17,29 @@ public class MainCtrl {
     @GetMapping("/calendario")
     public String showCalendario() {
         return "calendario";
+    }
+
+    @GetMapping("/login")
+    public String Acceso(Model modelo) {
+        return "login";
+    }
+
+    @PostMapping("/loginprueba")
+    public String comprobarAcceso(Model modelo, @RequestParam("usuario") String usu, @RequestParam("clave") String clave){
+        String texto = "Hola "+ usu +". Tu clave es " + clave + ".";
+        modelo.addAttribute("texto", texto);
+        return "exitoLogin";
+    }
+
+    @GetMapping("/registro")
+    public String registro(Model modelo) {
+
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String alta(Model modelo){
+
+        return "exitoLogin";
     }
 }
